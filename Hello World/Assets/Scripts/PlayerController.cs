e9dba70e6015cae119c1485f3d568b95448c858a
Hugo Whitacre <hugo4zelda@gmail.com>
2022-08-23 14:11:13 -0600

    Actually Added Unity FP

---
+++ Hello World/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public int speed;
+    public float hInput;
+    public float vInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hInput = Input.GetAxis("Horizontal");
+        vInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.right * speed * hInput * Time.deltaTime);
+        transform.Translate(Vector3.forward * speed * vInput * Time.deltaTime);
+    }
+}
