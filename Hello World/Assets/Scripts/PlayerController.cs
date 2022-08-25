30700ee3d40c3cec9c192dfdbb4f9204e8430b32
Hugo Whitacre <hugo4zelda@gmail.com>
2022-08-25 14:20:03 -0600

    Added Material and Updated Script

--- Hello World/Assets/Scripts/PlayerController.cs
+++ Hello World/Assets/Scripts/PlayerController.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public int speed;
-    public float hInput;
-    public float vInput;
+    public string playerName = "Kirby";
+
+    public int speed; // Set directional speed
+    public float hInput; // Variable for horizontal input
+    public float vInput; // Variable for vertical input
 
     // Start is called before the first frame update
     void Start()
@@ -17,9 +19,10 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        hInput = Input.GetAxis("Horizontal");
-        vInput = Input.GetAxis("Vertical");
+        hInput = Input.GetAxis("Horizontal"); // Input from keyboard
+        vInput = Input.GetAxis("Vertical"); // Input from keyboard
 
+        // Player move code along horizontal plane
         transform.Translate(Vector3.right * speed * hInput * Time.deltaTime);
         transform.Translate(Vector3.forward * speed * vInput * Time.deltaTime);
     }
