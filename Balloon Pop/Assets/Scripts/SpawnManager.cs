2bf3536e909d51d732260c2b05f6a8dfbde72c67
Hugo Whitacre <hugo4zelda@gmail.com>
2022-09-15 14:18:54 -0600

    Added a score manager
    Problems with showing the score.

--- Balloon Pop/Assets/Scripts/SpawnManager.cs
+++ Balloon Pop/Assets/Scripts/SpawnManager.cs
@@ -7,6 +7,7 @@ public class SpawnManager : MonoBehaviour
     public GameObject[] balloonPrefabs; // Array to store balloons
     public float startDelay = 0.5f;
     public float spawnInterval = 1.5f;
+    public float xRange = 16.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,11 +17,11 @@ public class SpawnManager : MonoBehaviour
     void SpawnRandomBalloon()
     {
         // Random position on x-axis
-        Vector3 spawnPos = new Vector3(Random.Range(-16,16),-16,0);
+        Vector3 spawnPos = new Vector3(Random.Range(-xRange,xRange),-16,0);
         // Pick random balloon from the balloon array
         int balloonIndex = Random.Range(-balloonPrefabs.Length,balloonPrefabs.Length);
-        // Code to make lower value balloons more common
         
+        // Code to make lower value balloons more common
         balloonIndex += Random.Range(-balloonPrefabs.Length,balloonPrefabs.Length);
         balloonIndex  = (balloonIndex / 2);
         if (balloonIndex < 0) balloonIndex *= -1;
