ae16c56cdbdee063a53e9d2ee4cfec392dac8a62
Hugo Whitacre <hugo4zelda@gmail.com>
2022-11-16 13:12:41 -0700

    Edited Health
    Bug fixes made to EnemyHealth.cs

--- Rogue Cultist/Assets/Scripts/EnemyHealth.cs
+++ Rogue Cultist/Assets/Scripts/EnemyHealth.cs
@@ -8,21 +8,38 @@ public class EnemyHealth : MonoBehaviour
     public int currentHealth;
     public float deathDelay;
 
+    private Renderer renderer;
+    private float blinkTime;
+    private float blink;
+
     // Start is called before the first frame update
     void Start()
     {
+        renderer = GetComponent<Renderer>();
         currentHealth = maxHealth;
+        blinkTime = 30 * Time.deltaTime;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if ((blink < blinkTime && blink > blinkTime * .7) || (blink < blinkTime * 0.5 && blink > blinkTime * 0.2))
+        {
+            renderer.material.color = Color.red;
+        }
+        else
+        {
+            renderer.material.color = Color.white;
+        }
+        blink -= 1;
     }
 
     public void TakeDamage(int dmgAmount)
     {
         currentHealth -= dmgAmount;
+        //renderer.material.color = Color.red;
+        //renderer.material.color = Color.white;
+
 
         if(currentHealth <= 0)
         {
@@ -31,6 +48,7 @@ public class EnemyHealth : MonoBehaviour
         }
     }
 
+    /*
     public void AddHealth(int healAmount)
     {
         currentHealth += healAmount;
@@ -40,4 +58,5 @@ public class EnemyHealth : MonoBehaviour
             currentHealth = maxHealth;
         }
     }
+    */
 }
